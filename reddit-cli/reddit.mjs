a51bec527197c9e87f45a7a45119ecd537b448de
Idowu Festus <40045755+Mcnoble1@users.noreply.github.com>
2022-05-16 00:27:49 +0100

    cli code :octocat:

--- reddit-cli/reddit.mjs
+++ reddit-cli/reddit.mjs
@@ -1,6 +1,24 @@
 #! /usr/bin/env node
+// import out packages
+import open from 'open';
+import fetch from 'node-fetch';
+import yargs from 'yargs';
 
-console.log('Hello from reddit cli'); 
+//  parse env variables
+ const {argv} = yargs(process.argv);
 
+ const res = await fetch('https://reddit.com/.json')
 
- 
+const data = await res.json();
+const children = data.data.children;
+const randomPost = children[Math.floor(Math.random() * children.length)];
+const link = `https://reddit.com/${randomPost.data.permalink}`;
+
+if(argv.print) {
+    console.log(`
+    Title: ${randomPost.data.title}
+    link: ${link}`)
+} else {
+    open(link)
+}
+
